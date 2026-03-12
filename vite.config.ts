a5522258d405fb437e63a897a744c3c8bfce701c
Olivia LaBorde <28545143+OliviaLaBorde@users.noreply.github.com>
2026-03-11 22:08:03 -0500

    vite config chg cuz it was mean

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => ({
+  base: '/ChromaSyn/',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'docs',
